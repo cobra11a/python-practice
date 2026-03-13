be4055285b8c9aa301cc5d1b969904a35e8fa1ef
cobra11a <rsudhanshu32@gmail.com>
2026-03-13 08:58:26 +0000

    update

--- practice.py
+++ practice.py
@@ -1,6 +1,32 @@
 import os 
 os.system('cls')
 
+# Customized Greeting Based on Time of Day
+# Create a variable user and assign the value 16
+
+# Use if-elif-else statements to print:
+
+# "Good Morning" if the hour is between 5 and 11,
+
+# "Good Afternoon" if the hour is between 12 and 17,
+
+# "Good Evening" if the hour is between 18 and 21,
+
+# "Good Night" for all other hours.
+
+# Print "Greeting code has completed."
+
+# Expected Output:
+
+# For input 10:
+
+# Good Morning
+# Greeting code has completed.
+# For input 15:
+
+# Good Afternoon
+# Greeting code has completed.
+
 hour = 16
 
 if 5 <= hour <= 11:
