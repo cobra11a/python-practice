44a612cd250b1169e2390a7db6fe6fa62f1b6836
cobra11a <rsudhanshu32@gmail.com>
2026-03-22 17:23:11 +0000

    Update

--- Selenium.py
+++ Selenium.py
@@ -15,17 +15,36 @@ driver = webdriver.Edge(service=service)
 
 driver.maximize_window()
 
-driver.get("https://demoqa.com/automation-practice-form")
+# driver.get("https://demoqa.com/automation-practice-form")
 
-driver.find_element(By.ID,"firstName").send_keys("Sudhanshu ")
-driver.find_element(By.ID,"userEmail").send_keys("sudhanshu.raj@gmail.com")
-driver.find_element(By.XPATH,"//input[@id='userNumber']").send_keys(8002444948)
+# driver.find_element(By.ID,"firstName").send_keys("Sudhanshu ")
+# driver.find_element(By.ID,"userEmail").send_keys("sudhanshu.raj@gmail.com")
+# driver.find_element(By.XPATH,"//input[@id='userNumber']").send_keys(8002444948)
 
 
 
-driver.find_element(By.ID,"gender-radio-2").click()
-driver.find_element(By.XPATH, "//input[@Class_Name ='btn']").click()
-driver.find_element(By.CSS_SELECTOR, "input[id ='currentAddress']").click()
+# driver.find_element(By.ID,"gender-radio-2").click()
+# driver.find_element(By.XPATH, "//input[@Class_Name ='btn']").click()
+# driver.find_element(By.CSS_SELECTOR, "input[id ='currentAddress']").click()
+
+# driver.get("https://demoqa.com/text-box")
+# time.sleep(2)
+# driver.find_element(By.ID ,"userName").send_keys("Sonu")
+# time.sleep(1)
+# driver.find_element(By.XPATH, "//button[@class='btn btn-primary']").click()
+# time.sleep(2)
+
+# text = driver.find_element(By.ID ,"name").text
+# time.sleep(2)
+
+# print(text)
+
+
+driver.get("https://demoqa.com/broken")
+time.sleep(2)
+
+driver.find_element(By.LINK_TEXT ,"Click Here for Valid Link").click()
+
 
 
 
@@ -90,10 +109,3 @@ time.sleep(5)
 
 
 
-
-
-
-
-
-
-time.sleep(2)
